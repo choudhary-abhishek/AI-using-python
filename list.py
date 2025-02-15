232121b6070b85da5290cbf8d4c2cd0c15ebdf92
Abhishek Choudhary <137060779+choudhary-abhishek@users.noreply.github.com>
2025-02-15 06:35:34 +0000

    update file code

--- list.py
+++ list.py
@@ -1,1 +1,23 @@
-print("hellow")
+my_list = [1,2,3]
+my_list.append(4)
+print(my_list)
+my_list.extend([3,4])
+print(my_list)
+my_list.insert(2,3)
+print(my_list)
+my_list.remove(2)
+print(my_list.pop(0))
+print(my_list[1:3])
+my_list.clear()
+print(my_list)
+print('sorting')
+my_list =[1,2,3,4]
+print(my_list[1:3])
+print(my_list.index(2))
+print(my_list.count(2))
+#sorting
+my_list = [3,1,4,2]
+my_list.sort()
+print(my_list)
+my_list.sort(reverse = True)
+print(my_list)
